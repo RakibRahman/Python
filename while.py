ac154d0ffd6b2f9b04a40c993b36441e3c725929
RakibRahman <md.rr.talukder@gmail.com>
2023-05-24 06:42:14 +0600

    loops

--- while.py
+++ while.py
@@ -1,5 +1,25 @@
 num = 5
 
-while num>0:
-    print(num)
-    num=num-1
+# while num > 0:
+#     print(num)
+#     num = num - 1
+
+# print("boom")
+# print(num)
+
+count = 3
+
+
+while True:
+    line = input("> ")
+    if line != "boom":
+        count -= 1
+    if line == "boom":
+        print("Bomb is triggered")
+        break
+    if count == 0:
+        print("Access denied,Exiting Program")
+        break
+    else:
+        print("Wrong keyword")
+        print("You have", count, "chances")
