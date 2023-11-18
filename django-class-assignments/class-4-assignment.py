32ed8b03980f9be06ff30d03052d022d1c28169b
rakibrahman <md.rr.talukder@gmail.com>
2023-11-18 18:09:16 +0600

    assignment of aiQuest class 4

--- django-class-assignments/class-4-assignment.py
+++ django-class-assignments/class-4-assignment.py
@@ -1,27 +1,31 @@
 store = list()
 
 while True:
-    operation = str(input('Chose Operation: \n 1 - CREATE\n 2 - READ\n 3 - UPDATE\n 4 - DELETE\n--->'))
-    print(operation)
-    if operation == '1' or operation == 'CREATE':
+    operation = str(input('Chose Operation: \n 1 - CREATE\n 2 - READ\n 3 - UPDATE\n 4 - DELETE\nType "exit" to close program \n---> '))
+
+    if operation == 'exit':
+        print('Program exited')
+        break
+    if operation == '1' or operation.upper() == 'CREATE':
         value = input('Enter Value: ')
         store.append(value)
-        print(store)
+        print(value,' is added!')
 
-    if operation == '2' or operation == 'READ':
-        print(store)
+    if operation == '2' or operation.upper() == 'READ':
+        print('Your Information==>',store)
 
-    if operation == '3' or operation == 'UPDATE':
-        currentValue = input('Enter Current Value: ')
+    if operation == '3' or operation.upper() == 'UPDATE':
+        currentValue = input('Enter value you want to update: ')
         if currentValue in store:
             index = store.index(currentValue)
             updateValue = input('Enter Update value: ')
             store[index] = updateValue
+            print('value is updated!')
         else:
-            print('Invalid value')
+            print('Value does not exist!')
 
-    if operation == '4' or operation == 'DELETE':
-        value = input('Enter Value: ')
+    if operation == '4' or operation.upper() == 'DELETE':
+        value = input('Enter Value you want to delete: ')
         store.remove(value)
-        print('delete opt')
+        print('value deleted')
 
